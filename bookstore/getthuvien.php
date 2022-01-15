19e5287c74ae0d0955a7a06e93834c4531affa82
vanhoan01 <92982928+vanhoan01@users.noreply.github.com>
2022-01-15 21:30:19 +0700

    update_php

--- bookstore/getthuvien.php
+++ bookstore/getthuvien.php
@@ -1,26 +1,24 @@
 <?php
     require "dbCon.php";
     $iduser = $_GET['iduser'];
-    $status = $_GET['status'];
-    if($status == 2){
-        $query = "SELECT receipt.productID, receipt.userID, product.name, productImg, concat(author.name, ', ') as authorname, status
-                FROM user
-                RIGHT JOIN receipt ON user.id = receipt.idUser 
-                LEFT JOIN product ON idProduct = product.id
-                LEFT JOIN authordetail ON product.id = authordetail.idProduct
-                LEFT JOIN author ON authordetail.idAuthor = author.id
-                WHERE user.id = '$iduser'
-                GROUP BY receipt.idProduct";
-    }else{
-        $query = "SELECT receipt.productID, receipt.userID, product.name, productImg, concat(author.name, ', ') as authorname, status
-                FROM user
-                RIGHT JOIN receipt ON user.id = receipt.idUser 
-                LEFT JOIN product ON idProduct = product.id
-                LEFT JOIN authordetail ON product.id = authordetail.idProduct
-                LEFT JOIN author ON authordetail.idAuthor = author.id
-                WHERE status = $status AND user.id = '$iduser'
-                GROUP BY receipt.idProduct";
+    $sx = $_GET['sx'];
+    //name purchaseTime authorname
+    if($sx == "purchaseTime"){
+        $sx = $sx." DESC";
     }
+    else{
+        $sx = $sx." ASC";
+    }
+    $query = "SELECT receipt.idProduct, receipt.idUser, product.name, productImg, concat(author.name, ', ') as authorname
+            FROM users
+            RIGHT JOIN receipt ON users.id = receipt.idUser 
+            LEFT JOIN product ON idProduct = product.id
+            LEFT JOIN authordetail ON product.id = authordetail.idProduct
+            LEFT JOIN author ON authordetail.idAuthor = author.id
+            WHERE users.id = $iduser
+            GROUP BY receipt.idProduct
+            ORDER BY $sx";
+    
     $data = mysqli_query($connect, $query);
     
     class Product{
@@ -29,20 +27,18 @@
         public $name;
         public $productImg;
         public $authorname;
-        public $status;
-        public function __construct($idProduct, $idUser, $name, $productImg, $authorname, $status){
+        public function __construct($idProduct, $idUser, $name, $productImg, $authorname){
             $this->idProduct = $idProduct;
             $this->idUser = $idUser;
             $this->name = $name;
             $this->productImg = $productImg;
             $this->authorname = $authorname;
-            $this->status = $status;
         }
     }
 
     $mangProduct = array();
     while($row = mysqli_fetch_assoc($data)){
-        $pd = new Product($row['idProduct'], $row['idUser'], $row['name'], $row['productImg'], $row['authorname'], $row['status']);
+        $pd = new Product($row['idProduct'], $row['idUser'], $row['name'], $row['productImg'], $row['authorname']);
         $mangProduct[] = $pd;
     }
 
